5fa8f9411e048163511f4fdb661494a1ad2490f8
testreview90-saw <testreview90@gmail.com>
2026-07-07 08:03:02 -0400

    Delete apt directory

--- apt/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation'
-import { getSession } from '@/lib/session'
-
-export default async function Home() {
-  const session = await getSession()
-  if (session.isLoggedIn) redirect('/dashboard')
-  else redirect('/login')
-}
